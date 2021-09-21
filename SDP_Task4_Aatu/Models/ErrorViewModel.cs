55919caeac5617d7d6604bc45bd32e92dc3bb3b5
supbrio <aatu_korhonen@hotmail.com>
2021-09-21 19:34:41 +0300

    Add project files.

---
+++ SDP_Task4_Aatu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SDP_Task4_Aatu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
